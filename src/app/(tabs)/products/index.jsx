000f1686ae85b811568bf86f2f1edbd02211c6c7
ronie-p <ronie.p@txtmequick.com>
2024-03-01 12:46:56 +0800

    added dummy product component

--- src/app/(tabs)/products/index.jsx
+++ src/app/(tabs)/products/index.jsx
@@ -1,13 +1,16 @@
 import { Link } from "expo-router";
 import { Text, View } from 'react-native';
+import { PRODUCT_LIST } from "../../../.data/data";
+import Product from "../../../components/Product";
 
 const addProduct = () => {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
+        <View className="flex-1 items-center bg-white">
             <Text className="text-red-400">Index of Product Page</Text>
             <Link href="/products/addProduct" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                 Add Product
             </Link>
+            {PRODUCT_LIST.map((product) => <Product />)}
         </View>
   )
 }
---
+++ src/components/Product.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Product = (props) => {
+  return (
+    <View>
+      <Text>Product</Text>
+    </View>
+  )
+}
+
+export default Product
