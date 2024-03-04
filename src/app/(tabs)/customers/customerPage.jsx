1cd102653b96e4d04be00e5acf208b34960f0579
ronie-p <ronie.p@txtmequick.com>
2024-03-05 00:16:52 +0800

    added date picker

---
+++ src/app/(tabs)/customers/customerPage.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const customerPage = () => {
+  return (
+    <View>
+      <Text>customerPage</Text>
+    </View>
+  )
+}
+
+export default customerPage
---
+++ src/app/(tabs)/home/salesForm.jsx
@@ -0,0 +1,165 @@
+import { } from "@react-native-community/datetimepicker";
+import { View, Text, TextInput, TouchableOpacity, ToastAndroid, Pressable, TouchableOpacityBase, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { useCallback, useRef, useState } from "react";
+import { useRealm } from "@realm/react";
+import { useForm, Controller } from 'react-hook-form';
+import { useLocalSearchParams, useNavigation } from "expo-router";
+import { BSON } from "realm";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import moment from "moment";
+
+
+const salesForm = () => {
+    const inputClass = "my-4 p-4 appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500";
+
+    const {  control, handleSubmit, formState: { errors }  } = useForm();
+    const realm = useRealm();
+    const navigation = useNavigation();
+    const params = useLocalSearchParams();
+
+    const refDateSI = useRef(null);
+    const refDateDelivered = useRef(null);
+
+    const handleSubmitCustomer = useCallback((data) => {
+        // try {
+        //     realm.write(() => {
+        //         let isNew = true;
+        //         const isCodeExist = (_code, _id) => {
+        //             const regexPattern = new RegExp(data.code, 'i');
+        //             const customer = realm.objects("customers").find((c) => regexPattern.test(c.code));
+        //             if (customer && _id)
+        //                 if (customer._id.toString() === _id) return false;
+        //             return !!customer;
+        //         };
+
+        //         if (isCodeExist(data.code, params._id))
+        //             throw new Error(`Code [${data.code}] already exist.`);
+
+        //         if (params._id) {
+        //             const customer = realm.objectForPrimaryKey("customers", new BSON.UUID(params._id));
+        //             if (customer) {
+        //                 customer.code = data.code;
+        //                 customer.name = data.name;
+        //                 customer.address = data.address;
+        //                 isNew = false;
+        //             }
+        //         }
+
+        //         if (isNew)
+        //             realm.create("customers", data);
+
+        //         ToastAndroid.show(`Customer ${isNew ? "created" : "updated"}.`, ToastAndroid.SHORT);
+
+        //         navigation.goBack();
+        //     });
+        // } catch (error) {
+        //     ToastAndroid.show(error.message || error, ToastAndroid.SHORT);
+        // }
+    }, [realm]);
+
+    const showDatePicker = (onChange = () => { }) => {
+        DateTimePickerAndroid.open({
+            value: new Date(),
+            mode: "date",
+            display: "calendar",
+            onChange: typeof onChange === "function" ? onChange : () => {}
+        });
+    };
+
+    return (
+        <View className="p-5">
+            <Text>New Sales</Text>
+
+            <Controller
+                control={control}
+                rules={{ required: false }}
+                name="dateOfSI"
+                defaultValue={params.dateOfSI || ""}
+                render={({ field: { value } }) => (
+                    <TouchableWithoutFeedback onPress={() => {
+                        Keyboard.dismiss();
+                        showDatePicker((_, date) => {
+                            if (refDateSI && refDateSI.current)
+                                refDateSI.current.setNativeProps({
+                                    text: moment(date).format("(dddd), MMMM DD, YYYY")
+                                });
+                        });
+                    }}>
+                        <View>
+                            <TextInput ref={refDateSI} className={inputClass} placeholder="Date Delivered" value={value} editable={false} />
+                        </View>
+                    </TouchableWithoutFeedback>
+                )}
+            />
+
+             <Controller
+                control={control}
+                rules={{ required: true }}
+                name="customerName"
+                defaultValue={params.name || ""}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput className={inputClass} autoCapitalize="characters" placeholder="Customer" onBlur={onBlur} onChangeText={onChange} value={value} />
+                )}
+            />
+
+             <Controller
+                control={control}
+                rules={{ required: false }}
+                name="poNo"
+                defaultValue={params.address || ""}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput className={inputClass} autoCapitalize="characters" placeholder="Purchase Order Number" onBlur={onBlur} onChangeText={onChange} value={value}  />
+                )}
+            />
+
+            <Controller
+                control={control}
+                rules={{ required: false }}
+                name="soNo"
+                defaultValue={params.address || ""}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput className={inputClass} autoCapitalize="characters" placeholder="Sales Order Number" onBlur={onBlur} onChangeText={onChange} value={value}  />
+                )}
+            />
+
+            <Controller
+                control={control}
+                rules={{ required: false }}
+                name="dateDelivered"
+                defaultValue={params.dateDelivered || ""}
+                render={({ field: { value } }) => (
+                    <TouchableWithoutFeedback onPress={() => {
+                        Keyboard.dismiss();
+                        showDatePicker((_, date) => {
+                            if (refDateDelivered && refDateDelivered.current)
+                                refDateDelivered.current.setNativeProps({
+                                    text: moment(date).format("(dddd), MMMM DD, YYYY")
+                                });
+                        });
+                    }}>
+                        <View>
+                            <TextInput ref={refDateDelivered} className={inputClass} placeholder="Date Delivered" value={value} editable={false} />
+                        </View>
+                    </TouchableWithoutFeedback>
+                )}
+            />
+
+            <TouchableOpacity className="p-4 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-full" onPress={handleSubmit(handleSubmitCustomer)}>
+                <Text className="text-white text-center text-[16px]">Save</Text>
+            </TouchableOpacity>
+
+             <Text>
+                {!!Object.keys(errors).length && JSON.stringify(errors)}
+            </Text>
+
+            {/* {showDatePicker && (
+                <DateTimePicker mode="date" display="spinner" value={date} onChange={(event, date) => {
+                    console.info({ event, date });
+                }} />
+            )} */}
+        </View>
+    );
+};
+
+
+export default salesForm;
