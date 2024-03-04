e49b6e7335ae537939cb638c0d36151bf1b7afc8
ronie-p <ronie.p@txtmequick.com>
2024-03-04 11:55:53 +0800

    added style for customers

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -6,9 +6,9 @@ const Customer = ({data, onEdit }) => {
     return (
     <View className="block w-full rounded-lg bg-white text-left p-2 my-2">
         <View className="p-4">
-            <Text className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-500">{data?.code || ""}</Text>
+            <Text className="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">{data?.code || ""}</Text>
             <Text className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">{data?.name || ""}</Text>
-            <Text className="block text-gray-700 text-base">{data?.address || ""}</Text>
+            <Text className="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">{data?.address || ""}</Text>
         </View>
         <View className="flex flex-row items-center justify-end border-t-2 border-neutral-100 font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">
                 <TouchableOpacity onPress={onEdit}>
