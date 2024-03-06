c023832cb2e6d2a0308ccfaa78358515ed32f463
ronie-p <ronie.p@txtmequick.com>
2024-03-07 00:34:28 +0800

    update home screen

--- src/app/(tabs)/home/index.jsx
+++ src/app/(tabs)/home/index.jsx
@@ -1,6 +1,6 @@
 import { router } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Pressable, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { ROUTES } from "../../../common/common";
 
 const Page = () => {
@@ -10,54 +10,19 @@ const Page = () => {
             <View className="basis-1/6">
 
             </View>
+            <View className="basis-1/5">
+                <View className="flex flex-row items-stretch w-full h-full rounded-lg">
+                    <Pressable className="m-2 flex-1 block rounded-lg bg-gray-200 items-center justify-center" onPress={() => router.navigate({ pathname: ROUTES.SALES })}>
+                        <Text className="text-slate-400 font-medium text-3xl">Sales</Text>
+                    </Pressable>
 
-            <View className="basis-1/6">
-
-            </View>
-            <View className="basis-2/6 border-2" >
-                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                    <View className="block rounded-lg text-left p-2 my-2 bg-white">
-                        <Text>Sales</Text>
-                    </View>
-                    <View className="block rounded-lg text-left p-2 my-2 bg-white">
-                        <Text>Collections</Text>
-                    </View>
-                    <View className="block rounded-lg text-left p-2 my-2 bg-white">
-                        <Text>Sales</Text>
-                    </View>
-                    <View className="block rounded-lg text-left p-2 my-2 bg-white">
-                        <Text>Collections</Text>
-                    </View>
-                     <View className="block rounded-lg text-left p-2 my-2 bg-white">
-                        <Text>Sales</Text>
-                    </View>
-                    <View className="block rounded-lg text-left p-2 my-2 bg-white">
-                        <Text>Collections</Text>
-                    </View>
-                     <View className="block rounded-lg text-left p-2 my-2 bg-white">
-                        <Text>Sales</Text>
-                    </View>
-                    <View className="block rounded-lg text-left p-2 my-2 bg-white">
-                        <Text>Collections</Text>
-                    </View>
-                </ScrollView>
+                    <Pressable className="m-2 flex-1 block rounded-lg bg-gray-200 items-center justify-center" onPress={() => router.navigate({ pathname: ROUTES.COLLECTIONS }) }>
+                        <Text className="text-slate-400 font-medium text-3xl">Collections</Text>
+                    </Pressable>
+                </View>
             </View>
 
-            <View className="flex gap-y-2 m-5 basis-1/6">
-                 <TouchableOpacity className="bg-blue-500 font-bold py-2 px-4 rounded-full" onPress={() => {
-                    router.navigate({ pathname: ROUTES.SALES });
-                }}>
-                    <Text className="text-white">Sales</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity className="bg-blue-500 font-bold py-2 px-4 rounded-full" onPress={() => {
-                    router.navigate({ pathname: ROUTES.COLLECTIONS });
-                }}>
-                    <Text className="text-white">Collections</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View className="basis-1/6">
+            <View className="h-full">
 
             </View>
 
