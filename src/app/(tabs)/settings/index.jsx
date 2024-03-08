eae70f185d749b9321822662a938f640ddf3f243
ronie-p <ronie.p@txtmequick.com>
2024-03-08 21:58:16 +0800

    added initial file reader

--- src/app/(tabs)/settings/index.jsx
+++ src/app/(tabs)/settings/index.jsx
@@ -1,9 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
+import { StorageAccessFramework } from "expo-file-system"
 
 const Page = () => {
     return (
         <View className="flex-1 items-center justify-center bg-white">
+            <Pressable onPress={() => {
+                StorageAccessFramework.requestDirectoryPermissionsAsync().then(async(permission) => {
+                    if (!permissions.granted) return;
+                     const { directoryUri } = permissions;
+                    const filesInRoot = await SAF.readDirectoryAsync(directoryUri);
+                    const filesInNestedFolder = await SAF.readDirectoryAsync(filesInRoot[0]);
+                    // Both values will be the same
+                    console.log({ filesInRoot, filesInNestedFolder })
+                })
+            }}>
+                <Text>Browse</Text>
+            </Pressable>
             <StatusBar style="auto" />
         </View>
   )
