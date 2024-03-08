11c02d06258e61c4e3d2e325a9f39b1ceb545bb5
ronie-p <ronie.p@txtmequick.com>
2024-03-08 16:33:57 +0800

    added absolute poath config

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,31 @@ module.exports = function (api) {
         plugins: [
             "nativewind/babel",
             ["@babel/plugin-proposal-decorators", { "legacy": true }],
-            'react-native-reanimated/plugin'
+            "react-native-reanimated/plugin",
+            [
+                "module-resolver",
+                {
+                    root: ["."],
+                    extensions: [
+                        '.ios.ts',
+                        '.android.ts',
+                        '.ts',
+                        '.ios.tsx',
+                        '.android.tsx',
+                        '.tsx',
+                        '.jsx',
+                        '.js',
+                        '.json',
+                    ],
+                    alias: {
+                        "@app": "./src/app",
+                        "@common": "./src/common",
+                        "@components": "./src/components",
+                        "@model": "./src/model",
+                        "@store": "./src/store",
+                    },
+                },
+            ],
         ],
     };
 };
