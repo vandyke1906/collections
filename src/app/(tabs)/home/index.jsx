901fdf01db2d9d628b9a015eb65f03ff6e7bad6a
ronie-p <ronie.p@txtmequick.com>
2024-03-09 18:05:45 +0800

    added count of customers and products

--- src/app/(tabs)/home/index.jsx
+++ src/app/(tabs)/home/index.jsx
@@ -9,6 +9,12 @@ const Page = () => {
 
     const salesInvoiceList = useQuery("salesInvoices");
     const collectionList = useQuery("collections");
+    const activeCustomers = useQuery("customers", (coll) => {
+        return coll.filtered("deletedAt == 0");
+    });
+    const activeProducts = useQuery("products", (coll) => {
+        return coll.filtered("deletedAt == 0");
+    });
 
     return (
         <View className="flex flex-col bg-white gap-y-2">
@@ -28,10 +34,23 @@ const Page = () => {
                 </View>
             </View>
             <View className="basis-1/5">
-                 <Pressable className="m-2 flex-1 block rounded-lg bg-gray-200 items-center justify-center" onPress={() => router.push({ pathname: ROUTES.FILTER_REPORT })}>
-                         <FontAwesome size={64} name="bar-chart" color="#189AB4" />
-                        <Text className="text-slate-900 font-medium text-center uppercase">View Reports</Text>
+                <View className="flex flex-row items-stretch w-full h-full rounded-lg">
+                    <Pressable className="m-2 flex-1 block rounded-lg bg-gray-200 items-center justify-center" onPress={() => router.push({ pathname: ROUTES.CUSTOMERS })}>
+                        <Text className="text-7xl font-sans text-blue-400">{activeCustomers.length}</Text>
+                        <Text className="text-slate-900 font-medium text-center">Customers</Text>
                     </Pressable>
+
+                    <Pressable className="m-2 flex-1 block rounded-lg bg-gray-200 items-center justify-center" onPress={() => router.push({ pathname: ROUTES.PRODUCTS })}>
+                        <Text className="text-7xl font-sans text-blue-400">{activeProducts.length}</Text>
+                        <Text className="text-slate-900 font-medium text-center">Products</Text>
+                    </Pressable>
+                </View>
+            </View>
+            <View className="basis-1/5">
+                <Pressable className="m-2 flex-1 block rounded-lg bg-gray-200 items-center justify-center" onPress={() => router.push({ pathname: ROUTES.FILTER_REPORT })}>
+                    <FontAwesome size={64} name="bar-chart" color="#189AB4" />
+                    <Text className="text-slate-900 font-medium text-center uppercase">View Reports</Text>
+                </Pressable>
             </View>
 
             <View className="h-full">
@@ -40,7 +59,7 @@ const Page = () => {
 
             <StatusBar style="auto" />
         </View>
-  )
-}
+    );
+};
 
-export default Page
+export default Page;
